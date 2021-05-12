b0c5c105bf5848599435ac92bfebf8a0a2a61ce2
tp <contribute@tensortable.com>
2021-05-12 08:03:40 +0100

    fix comments

--- pandas/core/indexes/numeric.py
+++ pandas/core/indexes/numeric.py
@@ -15,6 +15,7 @@ from pandas._libs import (
 from pandas._typing import (
     Dtype,
     DtypeObj,
+    final,
 )
 from pandas.util._decorators import (
     cache_readonly,
@@ -273,6 +274,15 @@ class NumericIndex(Index):
         """
         pass
 
+    @final
+    @cache_readonly
+    def _can_hold_na(self) -> bool:
+        if is_integer_dtype(self.dtype):
+            return False
+        else:
+            return True
+
+    @final
     @property
     def _is_all_dates(self) -> bool:
         """
